eb1f57bbda9aecd063d2c6817fc92a1566e58dce
robertblank <robert.blank@oxid-esales.com>
2019-01-30 15:43:10 +0100

    OXDEV-1820 Add PasswordServiceBridge

---
+++ source/Internal/Password/Bridge/PasswordServiceBridge.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Password\Bridge;
+
+use OxidEsales\EshopCommunity\Internal\Password\Service\PasswordHashServiceFactoryInterface;
+use OxidEsales\EshopCommunity\Internal\Password\Service\PasswordHashServiceInterface;
+
+/**
+ * @internal
+ */
+class PasswordServiceBridge implements PasswordServiceBridgeInterface
+{
+    /**
+     * @var PasswordHashServiceFactoryInterface
+     */
+    private $passwordHashServiceFactory;
+
+    /**
+     * @param PasswordHashServiceFactoryInterface $passwordHashServiceFactory
+     */
+    public function __construct(PasswordHashServiceFactoryInterface $passwordHashServiceFactory)
+    {
+        $this->passwordHashServiceFactory = $passwordHashServiceFactory;
+    }
+
+    /**
+     * @param string $algorithm
+     * @param array  $options
+     *
+     * @return PasswordHashServiceInterface
+     */
+    public function getPasswordHashService(string $algorithm, array $options): PasswordHashServiceInterface
+    {
+
+        return $this->passwordHashServiceFactory->getPasswordHashService($algorithm, $options);
+    }
+}
